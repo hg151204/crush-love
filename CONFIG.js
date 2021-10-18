c2f15ce42a7362002d003bb8279df83bd6cfb1fe
hg151204 <92728945+hg151204@users.noreply.github.com>
2021-10-18 05:10:40 -0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Có thích t không nà ?? 😂😂�😂",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+    "Nếu cậu thoát ra và không trả lời thì có nghĩa là hằng đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  buttonNo: "Không!",
+  titleModar: "Thank you <3 ",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Nói thế thôi chứ chúc hằng 20/10 vui vẽ, xinh đẹp <3 </br> "món quà nhỏ ^.^ "',
 };
